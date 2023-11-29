3fe95a0442d7639ac322343c388c6ef933b32bc3
devinareva <devinareva17@gmail.com>
2023-11-29 21:12:00 +0700

    add simpletask2

---
+++ simpletask_2/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, Docker!'
+ 
+@app.route('/home')
+def home():
+    return 'Ini adalah page /home'
+ 
+@app.route('/about')
+def about():
+    return 'Ini adalah page /about'
+ 
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0', port=5051)
